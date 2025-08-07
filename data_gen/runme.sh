b82dccb02d19e16904655881e32f05b64adb1a39
janakiramane138 <janakiramane138@gmail.com>
2025-08-07 15:00:01 -0500

    data_gen directory cleanup

--- data_gen/runme.sh
+++
@@ -1,7 +0,0 @@
-/mnt/c/Users/janak/OneDrive/Desktop/Fall 2025/VLSI CAD EDA/ir_solver_sparse.py
-/mnt/c/Users/janak/OneDrive/Desktop/Fall 2025/VLSI CAD EDA/data_generation_fixed.py
-/mnt/c/Users/janak/OneDrive/Desktop/Fall 2025/VLSI CAD EDA/training.py
-/mnt/c/Users/janak/OneDrive/Desktop/Fall 2025/VLSI CAD EDA/inference.py
-/mnt/c/Users/janak/OneDrive/Desktop/Fall 2025/VLSI CAD EDA/evaluate.py
-/mnt/c/Users/janak/OneDrive/Desktop/Fall 2025/VLSI CAD EDA/visualize_heatmap.py
-
--- data_gen/testcase_data_gen.sh
+++
@@ -1,6 +0,0 @@
-i=0
-for h in benchmarks/testcase*; do
-  i=$((i + 1))
-  echo "python3 ir_solver_sparse.py --input_file $h --output_file phase11_output/testcase${i}_out.v"
-done
-
